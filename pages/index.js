b582dd2f50b11ba7d3a5123caaf024c6628dceca
ashtonjo <hee576@nate.com>
2024-01-03 21:33:57 +0900

    style: 전체 코드 CSS 적용

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import ProductList from "@/components/ProductList";
-import SearchForm from "@/components/SearchForm.js";
-import styles from "@/styles/Home.module.css";
-import axios from "@/lib/axios.js";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import ProductList from '@/components/ProductList';
+import SearchForm from '@/components/SearchForm';
+import axios from '@/lib/axios';
+import styles from '@/styles/Home.module.css';
+import Header from '@/components/Header';
+import Container from '@/components/Container';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
-    const res = await axios.get("/products");
+    const res = await axios.get('/products');
     const nextProducts = res.data.results;
     setProducts(nextProducts);
   }
@@ -16,11 +18,14 @@ export default function Home() {
   useEffect(() => {
     getProducts();
   }, []);
+
   return (
     <>
-      <h1>AshtonMall</h1>
-      <SearchForm />
-      <ProductList products={products} />
+      <Header />
+      <Container>
+        <SearchForm />
+        <ProductList className={styles.products} products={products} />
+      </Container>
     </>
-  );
+  )
 }
