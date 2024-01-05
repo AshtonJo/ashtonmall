8a088d872ce4d16707727104a8a1723046235ca7
ashtonjo <hee576@nate.com>
2024-01-05 18:41:22 +0900

    feat: 다크모드,라이트모드 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import Container from "@/components/Container";
+import Head from "next/head";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <Head>
+        <title>AshtonMall</title>
+        <link rel="icon" href="/jessi.jpg"></link>
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import axios from "@/lib/axios";
 import styles from "@/styles/Search.module.css";
-import Header from "@/components/Header";
-import Container from "@/components/Container";
+
 /*
 검색 페이지에서는 검색어 값인 q 값을 기준으로 리퀘스트를 보냄.
 그래서 useEffect() 훅에서 q 값이 변할 때마다 getProducts() 함수를 실행
@@ -27,6 +27,9 @@ export default function Search() {
 
   return (
     <div>
+      <Head>
+        <title>{q} 검색결과 - AshtonMall</title>
+      </Head>
       <SearchForm initialValue={q} />
       <h2 className={styles.title}>
         <span className={styles.keyword}>{q}</span> 검색 결과
